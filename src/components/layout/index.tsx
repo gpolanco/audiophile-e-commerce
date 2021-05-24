97f366f09084f24304fbba6f0ad53d597a2af20c
Geordano Polanco <geordano.pr@gmail.com>
2021-05-24 22:31:07 +0200

    Configured scss module

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import React, { FC } from 'react';
 
-import styles from './layout.module.css';
+import styles from './layout.module.scss';
 
 interface ILayoutProps {}
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
 import { AppProps } from 'next/app';
+
 import { Layout } from '../components/layout';
+import '../styles/app.scss';
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
     return (
